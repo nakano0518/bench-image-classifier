6acefb6644b75478771021d26a18dd03e0f1e431
nakano0518 <m15sc018.prog@gmail.com>
2021-02-18 12:42:56 +0900

    [mod]app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 from PIL import Image
 from io import BytesIO
 import numpy as np
-from tensorflow.models import load_model
+from tensorflow.keras.models import load_model
 
 
 app = Flask(__name__)
